2d0bf4b0ad230e1c8e0ef270bb392d7c72784b06
Lee Hyeon Gyu <jeffsep24@gmail.com>
2022-10-17 19:16:26 +0900

    advC mook1 2-1

---
+++ advanced_C/mock1_2-1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int input(int *p);
+int *sel_next(int *p, int N, int M);
+int number(int *p, int *q);
+
+int main(){
+    int x[100], N = input(x), M, *q;
+
+    scanf("%d", &M);
+
+    printf("%d\n", number(x + M, sel_next(x, N, M)));
+
+    return 0;
+}
+
+int input(int *p){
+    int N = 0;
+    for(; ; p++, N++){
+        scanf("%d", p);
+        if(*p == -1) break;
+    }
+
+    return N + 1;
+}
+
+int *sel_next(int *p, int N, int M){
+    p += M;
+
+    for(int num = *p; num == *p; num++){
+        p++;
+    }
+
+    return p - 1;
+}
+
+int number(int *p, int *q){
+    int num = 0;
+
+    for(; p - 1 != q; p++){
+        num = num * 10 + *p;
+    }
+
+    return num;
+}
