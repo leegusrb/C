06591dbf508628fbf088db988cba11f1291af1fc
Lee Hyeon Gyu <jeffsep24@gmail.com>
2022-10-19 20:52:36 +0900

    update advC mock1 4-2

--- advanced_C/mock1/4-2.c
+++ advanced_C/mock1/4-2.c
@@ -4,41 +4,28 @@
 int main(){
     char A[100][251], B[100][101], tmp[251] = "";
 
-    gets(A[0]);
-    gets(B[0]);
-
-    int i1 = 1, j1 = 0;
-    for(char *p = A[0]; *p; p++){
-        if(*p == ' '){
-            A[i1++][j1] = '\0';
-            j1 = 0;
-        }else{
-            A[i1][j1++] = *p;
-        }
+    int i1 = 0;
+    while(1){
+        scanf("%s", A[i1++]);
+        if(getchar() == '\n') break;
     }
-    A[i1++][j1] = '\0';
 
-    int i2 = 1, j2 = 0;
-    for(char *p = B[0]; *p; p++){
-        if(*p == ' '){
-            B[i2++][j2] = '\0';
-            j2 = 0;
-        }else{
-            B[i2][j2++] = *p;
-        }
+    int i2 = 0;
+    while(1){
+        scanf("%s", B[i2++]);
+        if(getchar() == '\n') break;
     }
-    B[i2++][j2] = '\0';
 
-    int i = 1;
-    for(int x = 1; x < i1; x++){
-        for(int y = 1; y < x; y++){
+    int i = 0;
+    for(int x = 0; x < i1; x++){
+        for(int y = 0; y < x; y++){
             if(!strcmp(A[x], A[y])){
                 strcpy(A[x], B[i++]);
             }
         }
     }
 
-    for(int x = 1; x < i1; x++){
+    for(int x = 0; x < i1; x++){
         strcat(tmp, A[x]);
         strcat(tmp, " ");
     }
