a73da6b96e95c5993c36a230d38d20549a7b33c1
Lee Hyeon Gyu <jeffsep24@gmail.com>
2022-12-14 22:43:27 +0900

    algorithm 13-2 list

--- algorithm/13-2.c
+++ algorithm/13-2.c
@@ -11,14 +11,9 @@ typedef struct Edge {
     struct Edge *next;
 } Edge;
 
-typedef struct IncidentEdge {
-    Edge *e;
-    struct IncidentEdge *next;
-} IncidentEdge;
-
 typedef struct Vertex {
     int vName;
-    IncidentEdge *iHead;
+    int sack;
     struct Vertex *next;
 } Vertex;
 
@@ -31,19 +26,17 @@ int n, m;
 
 void initGraph(Graph *G);
 
-void makeVertex(Graph *G, int vName);
+void insertVertex(Graph *G, int vName);
 
 void insertEdge(Graph *G, int v1, int v2, int weight);
 
-void insertIncidentEdge(Vertex *v, Edge *e);
-
 Vertex *findVertex(Graph *G, int vName);
 
-int kruskalMST(Graph *G);
+void KruskalMST(Graph *G);
 
-Edge *findMinEdge(Graph *G);
+Edge *findMin(Graph *G);
 
-void merge(int *s, int u, int v);
+void merge(Graph *G, int v1, int v2);
 
 int main() {
     Graph *G = (Graph *) malloc(sizeof(Graph));
@@ -52,28 +45,30 @@ int main() {
     scanf("%d %d", &n, &m);
 
     for (int i = 1; i <= n; i++) {
-        makeVertex(G, i);
+        insertVertex(G, i);
     }
 
     for (int i = 0; i < m; i++) {
-        int v1, v2, w;
-        scanf("%d %d %d", &v1, &v2, &w);
-        insertEdge(G, v1, v2, w);
+        int v1, v2, weight;
+        scanf("%d %d %d", &v1, &v2, &weight);
+        insertEdge(G, v1, v2, weight);
     }
 
-    printf("%d\n", kruskalMST(G));
+    KruskalMST(G);
 
     return 0;
 }
 
+
 void initGraph(Graph *G) {
     G->vHead = NULL;
+    G->eHead = NULL;
 }
 
-void makeVertex(Graph *G, int vName) {
+void insertVertex(Graph *G, int vName) {
     Vertex *v = (Vertex *) malloc(sizeof(Vertex));
     v->vName = vName;
-    v->iHead = NULL;
+    v->sack = vName;
     v->next = NULL;
 
     Vertex *p = G->vHead;
@@ -95,102 +90,70 @@ void insertEdge(Graph *G, int v1, int v2, int weight) {
     e->isVisit = FALSE;
     e->next = NULL;
 
-    Edge *q = G->eHead;
-    if (q == NULL) {
-        G->eHead = e;
-    } else {
-        while (q->next) {
-            q = q->next;
-        }
-        q->next = e;
-    }
-
-    Vertex *p = findVertex(G, v1);
-    insertIncidentEdge(p, e);
-    p = findVertex(G, v2);
-    insertIncidentEdge(p, e);
-}
-
-void insertIncidentEdge(Vertex *v, Edge *e) {
-    IncidentEdge *i = (IncidentEdge *) malloc(sizeof(IncidentEdge));
-    i->e = e;
-    i->next = NULL;
-
-    IncidentEdge *p = v->iHead;
+    Edge *p = G->eHead;
     if (p == NULL) {
-        v->iHead = i;
+        G->eHead = e;
     } else {
         while (p->next) {
             p = p->next;
         }
-        p->next = i;
+        p->next = e;
     }
 }
 
 Vertex *findVertex(Graph *G, int vName) {
-    Vertex *p = G->vHead;
+    Vertex *v = G->vHead;
 
-    while (p && p->vName != vName) {
-        p = p->next;
+    while (v && v->vName != vName) {
+        v = v->next;
     }
 
-    return p;
+    return v;
 }
 
-int kruskalMST(Graph *G) {
-    int *s = (int *) malloc(sizeof(int) * (n + 1));
-
-    for (int i = 1; i <= n; i++) {
-        s[i] = i;
-    }
-
-    int tot = 0;
+void KruskalMST(Graph *G) {
     int cnt = 0;
+    int tot = 0;
     while (cnt < n - 1) {
-        Edge *q = findMinEdge(G);
-        q->isVisit = TRUE;
-        int u = findVertex(G, q->v1)->vName;
-        int v = findVertex(G, q->v2)->vName;
-        if (s[u] != s[v]) {
-            printf(" %d", q->weight);
-            tot += q->weight;
+        Edge *e = findMin(G);
+        e->isVisit = TRUE;
+        Vertex *v1 = findVertex(G, e->v1);
+        Vertex *v2 = findVertex(G, e->v2);
+        if (v1->sack != v2->sack) {
+            printf(" %d", e->weight);
             cnt++;
-            merge(s, s[u], s[v]);
+            tot += e->weight;
+            merge(G, v1->sack, v2->sack);
         }
     }
     printf("\n");
 
-    return tot;
+    printf("%d\n", tot);
 }
 
-Edge *findMinEdge(Graph *G) {
-    Edge *q = G->eHead;
+Edge *findMin(Graph *G) {
     Edge *min = NULL;
 
-    while (q) {
-        if (q->isVisit == FALSE) {
-            min = q;
+    for (Edge *p = G->eHead; p; p = p->next) {
+        if (p->isVisit == FALSE) {
+            min = p;
             break;
         }
-        q = q->next;
     }
 
-    q = min;
-
-    while (q) {
-        if (q->isVisit == FALSE && q->weight < min->weight) {
-            min = q;
+    for (Edge *p = min; p; p = p->next) {
+        if (p->isVisit == FALSE && p->weight < min->weight) {
+            min = p;
         }
-        q = q->next;
     }
 
     return min;
 }
 
-void merge(int *s, int u, int v) {
-    for (int i = 1; i <= n; i++) {
-        if (s[i] == v) {
-            s[i] = u;
+void merge(Graph *G, int v1, int v2) {
+    for (Vertex *p = G->vHead; p; p = p->next) {
+        if (p->sack == v2) {
+            p->sack = v1;
         }
     }
 }
