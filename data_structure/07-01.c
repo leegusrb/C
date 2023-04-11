3e210eb33aad5a617e27ff3f1f466ab4cc3d10be
Lee Hyeon Gyu <jeffsep24@gmail.com>
2023-04-11 11:05:11 +0900

    data structure week7

--- data_structure/07-01.c
+++ data_structure/07-01.c
@@ -10,57 +10,32 @@ typedef struct {
     Node *H;
 } List;
 
-void init(List *L);
+List *makeList() {
+    List *L = (List *) malloc(sizeof(List));
 
-void input(List *L, int n);
-
-void add(List *L, int e);
-
-int subset(List *A, List *B);
-
-int main() {
-    List *A = (List *) malloc(sizeof(List));
-    List *B = (List *) malloc(sizeof(List));
-    init(A);
-    init(B);
-
-    int n;
-
-    scanf("%d", &n);
-    input(A, n);
-
-    scanf("%d", &n);
-    input(B, n);
-
-    printf("%d\n", subset(A, B));
-
-    return 0;
-}
-
-void init(List *L) {
     L->H = NULL;
+
+    return (L);
 }
 
-void input(List *L, int n) {
-    for (int i = 0; i < n; i++) {
-        int e;
-        scanf("%d", &e);
-        add(L, e);
-    }
-}
-
-void add(List *L, int e) {
+Node *makeNode(int e) {
     Node *node = (Node *) malloc(sizeof(Node));
+
     node->elem = e;
     node->next = NULL;
+
+    return (node);
+}
+
+void append(List *L, int e) {
+    Node *node = makeNode(e);
 
     Node *p = L->H;
     if (!p) {
         L->H = node;
     } else {
-        while (p->next) {
+        while (p->next)
             p = p->next;
-        }
         p->next = node;
     }
 }
@@ -70,14 +45,35 @@ int subset(List *A, List *B) {
     Node *q = B->H;
 
     while (p) {
-        if (!q || p->elem < q->elem) {
-            return p->elem;
-        } else if (p->elem > q->elem) {
-            q = q->next;
-        } else {
+        if (!q || p->elem < q->elem)
+            return (p->elem);
+        
+        if (p->elem == q->elem)
             p = p->next;
-            q = q->next;
-        }
+        q = q->next;
     }
-    return 0;
+    return (0);
+}
+
+int main() {
+    List *A = makeList();
+    List *B = makeList();
+
+    int n, e;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &e);
+        append(A, e);
+    }
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &e);
+        append(B, e);
+    }
+
+    printf("%d", subset(A, B));
+
+    return (0);
 }
--- data_structure/07-02.c
+++ data_structure/07-02.c
@@ -10,109 +10,71 @@ typedef struct {
     Node *H;
 } List;
 
-void init(List *L);
-
-void input(List *L, int n);
-
-void add(List *L, int e);
-
-List *union_(List *A, List *B);
-
-List *intersect(List *A, List *B);
-
-void print(List *L);
-
-int main() {
-    List *A = (List *) malloc(sizeof(List));
-    List *B = (List *) malloc(sizeof(List));
-    init(A);
-    init(B);
-
-    int n;
-
-    scanf("%d", &n);
-    input(A, n);
-
-    scanf("%d", &n);
-    input(B, n);
-
-    List *U = union_(A, B);
-    List *I = intersect(A, B);
-
-    print(U);
-    print(I);
-
-    return 0;
-}
-
-void init(List *L) {
-    L->H = NULL;
-}
-
-void input(List *L, int n) {
-    for (int i = 0; i < n; i++) {
-        int e;
-        scanf("%d", &e);
-        add(L, e);
-    }
-}
-
-void add(List *L, int e) {
+Node *makeNode(int e) {
     Node *node = (Node *) malloc(sizeof(Node));
+
     node->elem = e;
     node->next = NULL;
 
+    return (node);
+}
+
+List *makeList() {
+    List *L = (List *) malloc(sizeof(List));
+
+    L->H = makeNode(0);
+
+    return (L);
+}
+
+void append(List *L, int e) {
+    Node *node = makeNode(e);
+
     Node *p = L->H;
-    if (!p) {
-        L->H = node;
-    } else {
-        while (p->next) {
-            p = p->next;
-        }
-        p->next = node;
-    }
+    while (p->next)
+        p = p->next;
+
+    p->next = node;
 }
 
 List *union_(List *A, List *B) {
-    List *U = (List *) malloc(sizeof(List));
-    init(U);
+    List *L = makeList();
 
-    Node *p = A->H;
-    Node *q = B->H;
+    Node *p = A->H->next;
+    Node *q = B->H->next;
 
     while (p && q) {
         if (p->elem < q->elem) {
-            add(U, p->elem);
+            append(L, p->elem);
             p = p->next;
         } else if (p->elem > q->elem) {
-            add(U, q->elem);
+            append(L, q->elem);
             q = q->next;
         } else {
-            add(U, p->elem);
+            append(L, p->elem);
             p = p->next;
             q = q->next;
         }
     }
 
     while (p) {
-        add(U, p->elem);
+        append(L, p->elem);
         p = p->next;
     }
 
     while (q) {
-        add(U, q->elem);
+        append(L, q->elem);
         q = q->next;
     }
 
-    return U;
+    return (L);
 }
 
 List *intersect(List *A, List *B) {
-    List *I = (List *) malloc(sizeof(List));
-    init(I);
+    List *L = makeList();
 
-    Node *p = A->H;
-    Node *q = B->H;
+    Node *p = A->H->next;
+    Node *q = B->H->next;
 
     while (p && q) {
         if (p->elem < q->elem) {
@@ -120,22 +82,47 @@ List *intersect(List *A, List *B) {
         } else if (p->elem > q->elem) {
             q = q->next;
         } else {
-            add(I, p->elem);
+            append(L, p->elem);
             p = p->next;
             q = q->next;
         }
     }
 
-    return I;
+    return (L);
 }
 
 void print(List *L) {
-    if (!L->H) {
-        printf("0");
-    }
+    Node *p = L->H->next;
 
-    for (Node *p = L->H; p; p = p->next) {
-        printf(" %d", p->elem);
+    if (!p) {
+        printf(" 0");
+    } else {
+        for (; p; p = p->next)
+            printf(" %d", p->elem);
     }
     printf("\n");
+}
+
+int main() {
+    List *A = makeList();
+    List *B = makeList();
+
+    int n, e;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &e);
+        append(A, e);
+    }
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &e);
+        append(B, e);
+    }
+
+    print(union_(A, B));
+    print(intersect(A, B));
+
+    return (0);
 }
